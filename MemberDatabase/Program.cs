85e79e1de6fbb8e87c40d1c0c3a837125705a254
Chris Evans <Cevans@peerlessproducts.com>
2019-09-11 21:55:21 -0500

    created console app and updated readme

---
+++ MemberDatabase/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace MemberDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Members of CIS-SO!");
+            Console.WriteLine();
+            foreach (var file in Directory.EnumerateFiles(@"..\..\..\data"))
+            {
+                var content = File.ReadAllText(file).Split(',');
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine($"First Name: {content[0]}");
+                Console.WriteLine($"Last Name: {content[1]}");
+                Console.WriteLine($"Favorite Language: {content[2]}");
+                Console.WriteLine($"What do you hope to get out of CIS-SO? {content[3]}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Welcome to CIS-SO!");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# ConsoleMemberDatabase
+# Console Member Database
+This is a simple dotnet core console application that is designed to get members familiar with adding code to github
+
+## Instructions
+
+- Open the program in visual studio
+- Create a new branch
+- Create a new file in the data folder.  Name it your first initial + last name (ex. Chris Evans = cevans.txt).
+- Add your first name, last name, favorite programming language, and what you hope to get out of CIS-SO (ex. Chris,Evans,C#)
+- Run the program and verify that it works
+- push your code and create a pull request
